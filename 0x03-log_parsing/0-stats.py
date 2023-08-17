3a15da42a4171544abeb2a049bb7f0e69328a8a6
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-17 15:55:15 +0100

    Add task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,45 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-"""
-This script reads input from stdin line by line, processes each line according to the specified format,
-and computes and prints metrics such as total file size and number of lines by status code.
-"""
-
-import signal
 import sys
+import signal
 
-status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-status_count = {code: 0 for code in status_codes}
-total_file_size = 0
-line_count = 0
+def print_metrics(metrics):
+    total_size = sum(metrics.values())
+    print(f"Total file size: File size: {total_size}")
+    for status_code in sorted(metrics.keys()):
+        print(f"{status_code}: {metrics[status_code]}")
 
-def print_statistics():
-    """
-    Print statistics including total file size and number of lines by status code.
-    """
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_codes):
-        if status_count[code] > 0:
-            print(f"{code}: {status_count[code]}")
-
-def process_line(line):
-    """
-    Process a single line and update metrics accordingly.
-
-    :param line: The input line to process.
-    """
-    global total_file_size, line_count
-    parts = line.split()
-    if len(parts) != 10:
-        return
-
-    _, _, _, _, _, _, status, size = parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7]
-    if status not in status_codes:
-        return
+def main():
+    metrics = {}
+    line_count = 0
 
     try:
-        size = int(size)
-    except ValueError:
-        return
+        for line in sys.stdin:
+            parts = line.strip().split()
 
-    status_count[status] += 1
-    total_file_size += size
-    line_count += 1
+            if len(parts) != 10:
+                continue
 
-    if line_count % 10 == 0:
-        print_statistics()
+            ip, _, _, date, _, _, request, status_code, file_size = parts
+            if not status_code.isdigit():
+                continue
 
-try:
-    for line in sys.stdin:
-        process_line(line.strip())
-except KeyboardInterrupt:
-    print_statistics()
+            status_code = int(status_code)
+            file_size = int(file_size)
+
+            metrics[status_code] = metrics.get(status_code, 0) + 1
+
+            if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
+                metrics[status_code] = metrics.get(status_code, 0) + 1
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_metrics(metrics)
+
+    except KeyboardInterrupt:
+        print_metrics(metrics)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
