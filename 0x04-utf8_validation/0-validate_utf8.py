4825081461defd3ceffe037da6224d257c92773a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-24 21:51:00 +0100

    Add task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,11 +5,30 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding
     """
-    for item in data:
-        for i in range(8):
-            item //= 2
-            if item == 0:
-                break
-        if item != 0:
-            return False
-    return True
+    byte_remain = 0
+
+    for byte in data:
+        if byte_remain == 0:
+            # Check for single-byte character
+            if byte >> 7 == 0:
+                byte_remain = 0
+            # Check for two-byte character
+            elif byte >> 5 == 0b110:
+                byte_remain = 1
+            # Check for single-byte character
+            elif byte >> 4 == 0b1110:
+                byte_remain = 2
+            # Check for single-byte character
+            elif byte >> 3 == 0b11110:
+                byte_remain = 3
+            #invalid byte pattern
+            else:
+                return False
+        else:
+            # Check for continuation byte
+            if byte >> 6 != 0b10:
+                # Invalid continuation byte
+                return False
+            byte_remain -= 1
+    #Put byte_remain = 0 to ensure all characters are properly formed
+    return byte_remain == 0
