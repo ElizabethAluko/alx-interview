fb0ad4ac7bb4a03175ccc4e8b19b3ec72c6696b6
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-24 21:56:31 +0100

    Add task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,23 +10,23 @@ def validUTF8(data):
     for byte in data:
         if byte_remain == 0:
             # Check for single-byte character
-            if byte >> 7 == 0:
+            if (byte >> 7) == 0:
                 byte_remain = 0
             # Check for two-byte character
-            elif byte >> 5 == 0b110:
+            elif (byte >> 5) == 0b110:
                 byte_remain = 1
             # Check for single-byte character
-            elif byte >> 4 == 0b1110:
+            elif (byte >> 4) == 0b1110:
                 byte_remain = 2
             # Check for single-byte character
-            elif byte >> 3 == 0b11110:
+            elif (byte >> 3) == 0b11110:
                 byte_remain = 3
             #invalid byte pattern
             else:
                 return False
         else:
             # Check for continuation byte
-            if byte >> 6 != 0b10:
+            if (byte >> 6) != 0b10:
                 # Invalid continuation byte
                 return False
             byte_remain -= 1
