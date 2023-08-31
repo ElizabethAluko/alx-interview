a58e9246a1ce7bb5c5f8f120169d44a55e783245
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-31 20:50:39 +0100

    Add task

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def is_safe(board, row, col, N):
     """
     Check if it's safe to place a queen at a given position.
@@ -18,16 +19,17 @@ def is_safe(board, row, col, N):
     for i in range(col):
         if board[row][i] == 1:
             return False
-        
+
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-        
+
     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-        
+
     return True
+
 
 def solve_nqueens_util(board, col, N):
     """
@@ -44,15 +46,16 @@ def solve_nqueens_util(board, col, N):
     if col >= N:
         print_solution(board, N)
         return True
-    
+
     res = False
     for i in range(N):
         if is_safe(board, i, col, N):
             board[i][col] = 1
             res = solve_nqueens_util(board, col + 1, N) or res
             board[i][col] = 0
-    
+
     return res
+
 
 def solve_nqueens(N):
     """
@@ -64,6 +67,7 @@ def solve_nqueens(N):
     board = [[0 for _ in range(N)] for _ in range(N)]
     if not solve_nqueens_util(board, 0, N):
         print("No solution exists for N =", N)
+
 
 def print_solution(board, N):
     """
@@ -80,6 +84,7 @@ def print_solution(board, N):
                 solution.append([i, j])
     print(solution)
 
+
 def main():
     """
     Main function to handle command-line arguments and solve N queens problem.
@@ -87,18 +92,19 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: ./0-nqueens.py N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     solve_nqueens(N)
+
 
 if __name__ == "__main__":
     main()
